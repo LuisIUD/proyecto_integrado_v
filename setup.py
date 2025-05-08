f248818e16a2577d54e60d27084cf3b202cee443
LuisIUD <85765282+LuisIUD@users.noreply.github.com>
2025-05-08 02:51:41 +0000

    setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="piv_2025",
     version="0.0.1",
-    author="Luis Pachon",
+    author="Luis Pachon - Cristhian Pachon",
     author_email="",
     description="",
     py_modules=["actividad_1","actividad_2"],
     install_requires=[
-        "pandas",
-        "numpy",
+        "pandas==2.2.3",
         "openpyxl",
-        "requests"
+        "requests==2.32.3",
+        "beautifulsoup4==4.13.3"
     ]
 ) 
